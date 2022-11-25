6370f72226ae2b44315b01f17d8531c8aa0d9937
emrergin <emrergin2757@yahoo.com>
2022-11-25 20:00:16 +0300

    small correction to result screen

--- src/components/ResultScreen.vue
+++ src/components/ResultScreen.vue
@@ -23,7 +23,7 @@
     <p>İkinci oyundan ödeme yapmak üzere {{ store.chosenRounds[1] }}. tur seçildi. O turdaki kazancınız: {{store.veriler[(store.veriler.length)/3+store.chosenRounds[1]-1][4]}}</p>
     <p>Üçüncü oyundan ödeme yapmak üzere {{ store.chosenRounds[2] }}. tur seçildi. O turdaki kazancınız: {{store.veriler[2*(store.veriler.length)/3+store.chosenRounds[2]-1][4]}}</p>
 
-    <p>Toplam kazancınız: {{ store.kazanc }}</p>
+    <p>Toplam puanınız: {{ store.kazanc }}</p>
     <p>Parasal kazancınız: {{ store.kazanc }}x 1,5 + 20 = {{store.kazanc*1.5+20}}</p>
     
   </div>
