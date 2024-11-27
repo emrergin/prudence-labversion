b9f1791f4cd41a06886f1b812b13d9bdcf2cb759
Emre Ergin <emrergin2757@yahoo.com>
2024-11-27 13:25:28 +0300

    treatment change

--- src/App.vue
+++ src/App.vue
@@ -132,8 +132,8 @@ export default {
         // },
       ],
       // traut || bleich || ours
-      experiment: "bleich",
-      // experiment: "traut",
+      // experiment: "bleich",
+      experiment: "traut",
     };
   },
   beforeMount() {
